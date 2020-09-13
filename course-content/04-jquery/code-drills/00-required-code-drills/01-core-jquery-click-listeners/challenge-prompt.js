fd7ee0e052903e6bf31658a0290e3033ffd01631
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-09-12 18:55:55 -0700

    Update challenge-prompt.js
    Wawahooooo....

--- course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
+++ course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
@@ -20,39 +20,126 @@
 // document.ready makes sure the javascript doesn't load before the html page
 $(document).ready(function () {
   <h1> "I'm tired of these motherloving snakes on this motherloving plane. Also, letters." <h1>
-  <h3 class="letterA"> A </h3>
-  <h3 class="letterB"> B </h3>
- <h3 class= "letterC"> C </h3>
- <h3 class= "letterD"> D </h3>
- <h3 class= "letterE"> E </h3>
- <h3 class= "letterF"> F </h3>
- <h3 class= "letterG"> G </h3>
- <h3 class= "letterH"> H </h3>
- <h3 class= "letterI"> I </h3>
- <h3 class= "letterJ"> J </h3>
- <h3 class= "letterK"> K </h3>
- <h3 class= "letterL"> L </h3>
- <h3 class= "letterM"> M </h3>
- <h3 class= "letterN"> N </h3>
- <h3 class= "letterO"> O </h3>
- <h3 class= "letterP"> P </h3>
- <h3 class= "letterQ"> Q </h3>
- <h3 class= "letterR"> R </h3>
- <h3 class= "letterS"> S </h3>
- <h3 class= "letterT"> T </h3>
- <h3 class= "letterU"> U </h3>
- <h3 class= "letterV"> V </h3>
- <h3 class= "letterW"> W </he>
- <h3 class= "letterX"> X </he>
- <h3 class= "letterY"> Y </he>
- <h3 class= "letterZ"> Z </he>
+  $(document).on("click", ".letterA", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("A");  <h3 class="letterA"> A </h3>
+  $(document).on("click", ".letterB", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("B");  <h3 class="letterB"> B </h3>
 <div id="text-div" style="height:10px;width:10px;"></div>
-
+ $(document).on("click", ".letterC", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("C");  <h3 class="letterB"> C </h3>
+   $(document).on("click", ".letterD", function () {
+        $("#text-div").append("D");  <h3 class="letterD"> D </h3><div id="text-div" style="height:10px;width:10px;"></div>
   <script src="challenge-prompt.js"></script>
-</body>
+        $(document).on("click", ".letterF", function () {
+        $("#text-div").append("F");  <h3 class="letterF"> F </h3><div id="text-div" style="height:10px;width:10px;"></div>
+         $(document).on("click", ".letterG", function () {
+        $("#text-div").append("G");  <h3 class="letterG"> G </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>
+   $(document).on("click", ".letterH", function () {
+        $("#text-div").append("H");  <h3 class="letterH"> H </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>
+   $(document).on("click", ".letterG", function () {
+        $("#text-div").append("G");  <h3 class="letterG"> H </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    //
 
-</html>
-    // Write a series of on click listeners for each letter in the html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>jQuery Click Listeners</title>
+
+  <!-- Added link to the jQuery Library -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<style>
+  h3 {
+    float: left;
+    padding: 5px;
+  }
+</style>
+<body>
+// document.ready makes sure the javascript doesn't load before the html page
+$(document).ready(function () {
+  <h1> "I'm tired of these motherloving snakes on this motherloving plane. Also, letters." <h1>
+  $(document).on("click", ".letterA", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("A");  <h3 class="letterA"> A </h3>
+  $(document).on("click", ".letterB", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("B");  <h3 class="letterB"> B </h3>
+<div id="text-div" style="height:10px;width:10px;"></div>
+ $(document).on("click", ".letterC", function () {
+        // When that element is clicked, append the string "A" to the `text-div` element
+        $("#text-div").append("C");  <h3 class="letterB"> C </h3>
+   $(document).on("click", ".letterD", function () {
+        $("#text-div").append("D");  <h3 class="letterD"> D </h3><div id="text-div" style="height:10px;width:10px;"></div>
+  <script src="challenge-prompt.js"></script>
+        $(document).on("click", ".letterF", function () {
+        $("#text-div").append("F");  <h3 class="letterF"> F </h3><div id="text-div" style="height:10px;width:10px;"></div>
+         $(document).on("click", ".letterG", function () {
+        $("#text-div").append("G");  <h3 class="letterG"> G </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>
+   $(document).on("click", ".letterH", function () {
+        $("#text-div").append("H");  <h3 class="letterH"> H </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>
+   $(document).on("click", ".letterI", function () {
+        $("#text-div").append("I");  <h3 class="letterI"> I </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterJ", function () {
+        $("#text-div").append("J");  <h3 class="letterJ"> J </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterK", function () {
+        $("#text-div").append("K");  <h3 class="letterK"> K </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterL", function () {
+        $("#text-div").append("L");  <h3 class="letterI"> L </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterM", function () {
+        $("#text-div").append("M");  <h3 class="letterM"> M </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterN", function () {
+        $("#text-div").append("N");  <h3 class="letterN"> N</h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterO", function () {
+        $("#text-div").append("O");  <h3 class="letterO"> O </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterP", function () {
+        $("#text-div").append("P");  <h3 class="letterP"> P </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterQ", function () {
+        $("#text-div").append("Q");  <h3 class="letterQ"> Q </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterR", function () {
+        $("#text-div").append("R");  <h3 class="letterR"> R </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterS", function () {
+        $("#text-div").append("S");  <h3 class="letterS"> S </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterT", function () {
+        $("#text-div").append("T");  <h3 class="letterI"> T </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the htmlenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
+$(document).on("click", ".letterU", function () {
+        $("#text-div").append("U");  <h3 class="letterU"> U </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterV", function () {
+        $("#text-div").append("V");  <h3 class="letterV"> V </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html// You will need to set up an on click listener for each letter
+$(document).on("click", ".letterW", function () {
+        $("#text-div").append("W");  <h3 class="letterW"> W </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html//
+$(document).on("click", ".letterX", function () {
+        $("#text-div").append("X");  <h3 class="letterX"> X </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterY", function () {
+        $("#text-div").append("Y");  <h3 class="letterY"> Y </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+$(document).on("click", ".letterZ", function () {
+        $("#text-div").append("Z");  <h3 class="letterZ"> Z </h3><div id="text-div" style="height:10px;width:10px;"></div>  <script src="challenge-prompt.js"></script></body>    // Write a series of on click listeners for each letter in the html
+          
+          
+          // Within the document, set an on click listener for the element with a class of "letterA"
+
+    
+        // When that element is clicked, append the string "A" to the `text-div` element
+
+        
+    // Within the document, set an on click listener for the element with a class of "letterB"
+
+    
+        // When that element is clicked, append the string "B" to the `text-div` element
+
+        
+    // Continue for the other letters
+
+
+
+
+        
+})
+
     // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
     // You will need to set up an on click listener for each letter
     //
