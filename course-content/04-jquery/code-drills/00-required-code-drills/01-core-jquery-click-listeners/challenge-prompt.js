c0d2ffbfdbd03ab2037d08fafc6fed6ca78139d9
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-09-12 18:11:02 -0700

    Update challenge-prompt.js
    Failing since 1985...

--- course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
+++ course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
@@ -1,7 +1,26 @@
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <title>jQuery Click Listeners</title>
+
+  <!-- Added link to the jQuery Library -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+<style>
+  h3 {
+    float: left;
+    padding: 5px;
+  }
+</style>
+<body>
 // document.ready makes sure the javascript doesn't load before the html page
 $(document).ready(function () {
+  <h1> "I'm tired of these motherloving snakes on this motherloving plane. Also, letters." <h1>
+  <h3> a
 
     // Write a series of on click listeners for each letter in the html
     // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
