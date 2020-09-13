e45fc33c27ed97004f7271933f71dbdcb3520baf
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-09-12 18:31:56 -0700

    Update challenge-prompt.js
    Got rid of the right carrots. I am so faking it until I make it. Blarg.

--- course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
+++ course-content/04-jquery/code-drills/00-required-code-drills/01-core-jquery-click-listeners/challenge-prompt.js
@@ -22,31 +22,36 @@ $(document).ready(function () {
   <h1> "I'm tired of these motherloving snakes on this motherloving plane. Also, letters." <h1>
   <h3 class="letterA"> A </h3>
   <h3 class="letterB"> B </h3>
- <h3> class= "letterC"> C </h3>
- <h3> class= "letterD"> D </h3>
- <h3> class= "letterE"> E </h3>
- <h3> class= "letterF"> F </h3>
- <h3> class= "letterG"> G </h3>
- <h3> class= "letterH"> H </h3>
- <h3> class= "letterI"> I </h3>
- <h3> class= "letterJ"> J </h3>
- <h3> class= "letterK"> K </h3>
- <h3> class= "letterL"> L </h3>
- <h3> class= "letterM"> M </h3>
- <h3> class= "letterN"> N </h3>
- <h3> class= "letterO"> O </h3>
- <h3> class= "letterP"> P </h3>
- <h3> class= "letterQ"> Q </h3>
- <h3> class= "letterR"> R </h3>
- <h3> class= "letterS"> S </h3>
- <h3> class= "letterT"> T </h3>
- <h3> class= "letterU"> U </h3>
- <h3> class= "letterV"> V </h3>
- <h3> class= "letterW"> W </he>
- <h3> class= "letterX"> X </he>
- <h3> class= "letterY"> Y </he>
- <h3> class= "letterZ"> Z </he>
- 
+ <h3 class= "letterC"> C </h3>
+ <h3 class= "letterD"> D </h3>
+ <h3 class= "letterE"> E </h3>
+ <h3 class= "letterF"> F </h3>
+ <h3 class= "letterG"> G </h3>
+ <h3 class= "letterH"> H </h3>
+ <h3 class= "letterI"> I </h3>
+ <h3 class= "letterJ"> J </h3>
+ <h3 class= "letterK"> K </h3>
+ <h3 class= "letterL"> L </h3>
+ <h3 class= "letterM"> M </h3>
+ <h3 class= "letterN"> N </h3>
+ <h3 class= "letterO"> O </h3>
+ <h3 class= "letterP"> P </h3>
+ <h3 class= "letterQ"> Q </h3>
+ <h3 class= "letterR"> R </h3>
+ <h3 class= "letterS"> S </h3>
+ <h3 class= "letterT"> T </h3>
+ <h3 class= "letterU"> U </h3>
+ <h3 class= "letterV"> V </h3>
+ <h3 class= "letterW"> W </he>
+ <h3 class= "letterX"> X </he>
+ <h3 class= "letterY"> Y </he>
+ <h3 class= "letterZ"> Z </he>
+<div id="text-div" style="height:10px;width:10px;"></div>
+
+  <script src="challenge-prompt.js"></script>
+</body>
+
+</html>
     // Write a series of on click listeners for each letter in the html
     // Whenever the user clicks on a letter, you want to append the corresponding letter to the `text-div` element
     // You will need to set up an on click listener for each letter
