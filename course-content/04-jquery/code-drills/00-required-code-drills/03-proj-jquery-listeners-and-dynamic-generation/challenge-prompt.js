078b85cf99c4bedd5c658f4b1b2bb8f70e672648
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-09-13 16:21:41 -0700

    Update challenge-prompt.js
    Okay, I got through two of them. Haven't checked solved yet.

--- course-content/04-jquery/code-drills/00-required-code-drills/03-proj-jquery-listeners-and-dynamic-generation/challenge-prompt.js
+++ course-content/04-jquery/code-drills/00-required-code-drills/03-proj-jquery-listeners-and-dynamic-generation/challenge-prompt.js
@@ -3,21 +3,44 @@ $(document).ready(function () {
     // Section 1 creating the array and variables
   
     // 1.1 create a variable called letters with an array of every letter in alphabet (A-Z) as a string
+       var=lettters;
+       letters("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P")
     
 
     // 1.2 create a variable called body and have it equals the jquery selector of body
+       
+       var=body;
+       body==$("body");
+       var=btns
+       btns==$("btns")
+       $("btns")=$("body");
+    
+    
 
   
     // 1.3 create a variable called btns and assign the result of calling the "find" method on the body variable to find the element with an id of "buttons"
-
+        var=btns
+     $("li.item-ii").find(body);
+     $("li.item-ii").find(btns);
+     $("li.item.btns).find(buttons);
+     
   
     // Section 2 : DYNAMICALLY the text on the button
     // =================================================================================
   
     // 2.1 Create a for-loop that iterate through the variable letters.
+       
+       var arr=[b,u,t,t,o,n,s];
+       
 
   
       // 2.2 Create a variable named "textButton" equal to a jquery selctor of a string <button>
+       
+       var=textButton= $(textButton);
+       textButton(t,e,x,t,b,u,t,t,o,n);
+       textButton="data-letter";
+       textButton=dataLetter(d,a,t,a,l,e,t,t,e,r);
+       btns=textButton;
 
   
       // 2.3. use the variable textButton add following class as a string letter-button 
@@ -37,7 +60,7 @@ $(document).ready(function () {
     // Section 3 : attaching the button text to a div
     // =================================================================================
   
-    // 3.1 Create an "on-click" event attached to the ".letter-button" class.
+   
 
   
       // 3.2 Create a variable called textDiv and and set it equal to jquery selector string of <div>.
