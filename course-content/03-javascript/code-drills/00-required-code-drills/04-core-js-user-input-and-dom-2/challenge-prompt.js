db14de4cbcf2c68d5dc22ef41cb978e1651fdcdb
Franz Iza Badsho <66654951+KHulsy@users.noreply.github.com>
2020-11-23 17:27:09 -0800

    Update challenge-prompt.js

--- course-content/03-javascript/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
+++ course-content/03-javascript/code-drills/00-required-code-drills/04-core-js-user-input-and-dom-2/challenge-prompt.js
@@ -4,13 +4,26 @@
 
 // Using the `confirm` method, welcome the user to the Boba Shop and ask them if they would like a boba
 // Store the user response in a variable `wantsBoba`
+confirm("Welcome to the Boba Shop, Mein Herr! Quieres Boba?);
+    var=wantsBoba
+     
 
 
 // Create an if statement checking whether the user wanted boba from the earlier `confirm` dialogue
+        
+        if(userwantedBoba) { "Here's All The Boba!"
+        else(userdidnotwantBobabecausetheyreacommunist){"No Boba for you, Pinko!};
+                                                        
 
 
 
     // Declare a variable `drinkFlavor`
+       var="drinkFlavor"
+       confirm("Welcome to the Soda Shop, Senor Gato Pantalones! What's your poison?");
+       drinkFlavor("SparklyAbsinthe");
+       if(drinkFlavor=("SparklyAbsinthe"), then ("Your drink will be ready soon, Mon Ami!")
+       else(drinkFlavor=("RootBeer"), then ("Goodbye! Come back soon!");
+                                                        
 
     
     // Prompt the user for what flavor they would like for their drink
